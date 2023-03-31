1cfcc99d7164aa06756606c0cdef1f4627898e9d
Daniel Fajardo <danielf.fajardop30@gmail.com>
2023-03-31 08:13:36 -0500

    Actualización de dependencias

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import boto3
+import os
+import pandas as pd
+from datetime import datetime
+
+def lambda_handler():
+    # Obtener las credenciales de AWS
+    aws_access_key_id = os.environ.get('MY_AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.environ.get('MY_AWS_SECRET_ACCESS_KEY')
+
+    dynamo_client = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    dynamo_response = dynamo_client.scan(TableName='sample')
+    df = pd.DataFrame.from_dict(dynamo_response['Items'])
+    df = df.applymap(lambda x: list(x.values())[0]).astype(str)
+
+    # Define el nombre del archivo CSV de salida
+    filename = f'archivo-salida-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    s3_client.put_object(Body=df.to_csv(index=False), Bucket='sample-s3-dynamo', Key=filename)
--- main.py
+++
@@ -1,4 +0,0 @@
-from dynamo_to_s3 import dynamo_to_s3
-
-# Llamar a la función con un argumento
-dynamo_to_s3(table_name='sample', bucket_name='sample-s3-dynamo')
